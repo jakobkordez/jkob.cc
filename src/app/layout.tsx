e230be2c163ca15e3a5ec0778f482251c17c0430
Jakob Kordež <jakobkordez1999@gmail.com>
2023-03-04 14:50:38 +0100

    Update robots

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,9 @@ export const metadata: Metadata = {
     googleBot: {
       index: true,
       follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
     },
   },
   verification: {
